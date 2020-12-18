6c1a5ed87a55a3bd1d88a618988857356ccc55f4
sahithi2604 <37655909+sahithi2604@users.noreply.github.com>
2020-12-18 11:59:45 +0530

    changed

--- simplesoap/src/com/lti/services/Hello.java
+++ simplesoap/src/com/lti/services/Hello.java
@@ -3,7 +3,7 @@ package com.lti.services;
 public class Hello {
 	
 	public String sayHello(String name) {
-		return "Hello" + name +" from web services";
+		return "Hello" + name +" from web ";
 	}
 
 }
